9859773996450899f283ff57b89aaf6321a72188
Christian <c.espinoza.code@gmail.com>
2022-11-27 19:18:51 -0500

    onScroll event initialized

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,36 @@
 import { Component } from '@angular/core';
 
+/* AppComponent class properties need to be public */
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'my-project';
+  name: string = 'Christian'
+  age: number = 24
+  imgAngular: string = 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/cf/Angular_full_color_logo.svg/1200px-Angular_full_color_logo.svg.png'
+  imgDocker: string = 'https://miro.medium.com/max/323/1*DPjl0PU5oGDDhajYoof38w.png'
+  btnDisabled: boolean = false
+  progressValue = 50
+  /* Using objects */
+  person = {
+    name: 'Nicolas',
+    lastName: 'Rojas',
+    avatar: 'https://us.123rf.com/450wm/redlinevector/redlinevector1704/redlinevector170402590/75360779-icono-simple-del-programador.jpg?ver=6'
+  }
+
+  increaseAge() {
+    this.age += 1
+  }
+
+  toggleButton() {
+    this.btnDisabled = !this.btnDisabled
+  }
+
+  /* Capturar el evento */
+  onScroll(event: Event) {
+
+  }
 }
