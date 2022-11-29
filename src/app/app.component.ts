fbda2a0d45c289baf64b6057abbc55603b2852f2
Christian <c.espinoza.code@gmail.com>
2022-11-28 20:29:22 -0500

    coleccion de albumes object created

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -40,6 +40,11 @@ export class AppComponent {
       price: 300,
       image: './assets/images/bicicleta.jpg'
     },
+    {
+      name: 'Colección de albumes',
+      price: 300,
+      image: './assets/images/albumes.jpg'
+    },
   ]
 
   /* Métodos */
